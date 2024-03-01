39234528e196997e5d0802cdc348222839c8353c
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-02-29 23:38:03 -0600

    Added initial frontend structure and styling

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
+const PORT = process.env.PORT || 8000;
 
 // Always require and configure near the top
 require('dotenv').config()
 
-const app = express()
+// Connect to the database
+require('./config/database')
 
-app.use(logger('dev'))
 app.use(express.json())
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
+app.use(logger('dev'))
 
 // Configure both serve-favicon & statuc middleware
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+
+app.use(express.static('public'))
+
+// for react router
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(path.join(__dirname, 'public', 'index.html')))
+})
+
+app.listen(PORT, () => {
+	console.log('App is listening on PORT ' + PORT)
+})
+
+module.exports = app
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,29 @@
+import {  useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+// Component Imports
+import NavBar from '../../components/NavBar/NavBar'
+// Page Imports
+import AuthPage from '../AuthPage/AuthPage'
+import NewOrderPage from '../NewOrderPage/NewOrderPage'
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
 
 export default function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <main className="App">
-      App
+      { user ?
+        <>
+          <NavBar />
+          <Routes>
+            <Route path='/orders/new' element={ <NewOrderPage /> } />
+            <Route path='/orders' element={ <OrderHistoryPage /> } />
+          </Routes>
+        </>
+        :
+        <AuthPage />
+      }
     </main>
   );
 }
